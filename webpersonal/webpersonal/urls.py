088bdbd0840850cace5b2f166760cf731825606b
AlbertoDeTeresaChaves <alberto.trabajo.es@gmail.com>
2024-01-12 10:48:56 +0100

    agregando contactos

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -21,5 +21,6 @@ from core import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('about/',views.about,name="about"),
+    path('contact/',views.contact,name="contact"),
     path('admin/', admin.site.urls),
 ]
